9e72647775d9e62340c5a8b1bfeb91a89bda05ac
4497cv <4497cv@gmail.com>
2019-11-21 18:01:36 -0600

    initial commit

---
+++ Debug/framework/Messaging/Source/Messaging.d
@@ -0,0 +1,42 @@
+framework/Messaging/Source/Messaging.o \
+ framework/Messaging/Source/Messaging.d: \
+ ../framework/Messaging/Source/Messaging.c \
+ C:/workspace_rse/frdmkw41z_wireless_examples_thread_router_eligible_device_freertos/source/config.h \
+ ../nwk_ip/app/config/app_config.h \
+ ../nwk_ip/app/config/app_stack_config.h \
+ ../nwk_ip/core/interface/modules/stack_config.h \
+ ../nwk_ip/core/interface/modules/thread_stack_config.h \
+ ../nwk_ip/app/config/app_framework_config.h \
+ ../nwk_ip/app/config/app_mac_config.h \
+ ../framework/common/EmbeddedTypes.h \
+ ../framework/MemManager/Interface/MemManager.h \
+ ../framework/Lists/GenericList.h \
+ ../framework/Messaging/Interface/Messaging.h \
+ ../framework/OSAbstraction/Interface/fsl_os_abstraction.h \
+ ../framework/OSAbstraction/Interface/fsl_os_abstraction_config.h
+
+C:/workspace_rse/frdmkw41z_wireless_examples_thread_router_eligible_device_freertos/source/config.h:
+
+../nwk_ip/app/config/app_config.h:
+
+../nwk_ip/app/config/app_stack_config.h:
+
+../nwk_ip/core/interface/modules/stack_config.h:
+
+../nwk_ip/core/interface/modules/thread_stack_config.h:
+
+../nwk_ip/app/config/app_framework_config.h:
+
+../nwk_ip/app/config/app_mac_config.h:
+
+../framework/common/EmbeddedTypes.h:
+
+../framework/MemManager/Interface/MemManager.h:
+
+../framework/Lists/GenericList.h:
+
+../framework/Messaging/Interface/Messaging.h:
+
+../framework/OSAbstraction/Interface/fsl_os_abstraction.h:
+
+../framework/OSAbstraction/Interface/fsl_os_abstraction_config.h:
---
+++ Debug/framework/NVM/Source/NV_FsciCommands.d
@@ -0,0 +1,42 @@
+framework/NVM/Source/NV_FsciCommands.o \
+ framework/NVM/Source/NV_FsciCommands.d: \
+ ../framework/NVM/Source/NV_FsciCommands.c \
+ C:/workspace_rse/frdmkw41z_wireless_examples_thread_router_eligible_device_freertos/source/config.h \
+ ../nwk_ip/app/config/app_config.h \
+ ../nwk_ip/app/config/app_stack_config.h \
+ ../nwk_ip/core/interface/modules/stack_config.h \
+ ../nwk_ip/core/interface/modules/thread_stack_config.h \
+ ../nwk_ip/app/config/app_framework_config.h \
+ ../nwk_ip/app/config/app_mac_config.h \
+ ../framework/common/EmbeddedTypes.h \
+ ../framework/Messaging/Interface/Messaging.h \
+ ../framework/Lists/GenericList.h \
+ ../framework/MemManager/Interface/MemManager.h \
+ ../framework/FunctionLib/FunctionLib.h \
+ ../framework/NVM/Interface/NVM_Interface.h
+
+C:/workspace_rse/frdmkw41z_wireless_examples_thread_router_eligible_device_freertos/source/config.h:
+
+../nwk_ip/app/config/app_config.h:
+
+../nwk_ip/app/config/app_stack_config.h:
+
+../nwk_ip/core/interface/modules/stack_config.h:
+
+../nwk_ip/core/interface/modules/thread_stack_config.h:
+
+../nwk_ip/app/config/app_framework_config.h:
+
+../nwk_ip/app/config/app_mac_config.h:
+
+../framework/common/EmbeddedTypes.h:
+
+../framework/Messaging/Interface/Messaging.h:
+
+../framework/Lists/GenericList.h:
+
+../framework/MemManager/Interface/MemManager.h:
+
+../framework/FunctionLib/FunctionLib.h:
+
+../framework/NVM/Interface/NVM_Interface.h:
